3bde06f9faa31ee1f4b181b499ce3e0e3b894a31
Gianluca Fabrizi <info@gianlucafabrizi.it>
2020-02-03 00:18:11 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { Application } from '@pixi/app'
+
+const app = new Application({
+    width: 100,
+    height: 100
+});
+
+document.body.appendChild(app.view);
---
+++ webpack.config.prod.js
@@ -0,0 +1,37 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+    mode: 'production',
+    module: {
+        rules: [{
+            test: /\.(js)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6,
+            },
+        })],
+    },
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: 'build/assets',
+            to: 'assets'
+        }]),
+        new HTMLWebpackPlugin({
+            template: 'build/index.html',
+            filename: 'index.html',
+            hash: true,
+            minify: false
+        })
+    ]
+}
